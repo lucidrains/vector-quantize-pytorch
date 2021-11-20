e562e0fd4f0ef0125f4fdf97d242845e78a4492d
Phil Wang <lucidrains@gmail.com>
2021-11-19 17:08:28 -0800

    0.3.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'vector_quantize_pytorch',
   packages = find_packages(),
-  version = '0.3.9',
+  version = '0.3.10',
   license='MIT',
   description = 'Vector Quantization - Pytorch',
   author = 'Phil Wang',
