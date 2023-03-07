8f38abb8f42aeee54950d92c3d78fba98e6b6ce8
Phil Wang <lucidrains@gmail.com>
2023-03-06 17:36:30 -0800

    add random projection quantizer, used by Google USM

--- vector_quantize_pytorch/__init__.py
+++ vector_quantize_pytorch/__init__.py
@@ -1,3 +1,3 @@
 from vector_quantize_pytorch.vector_quantize_pytorch import VectorQuantize
 from vector_quantize_pytorch.residual_vq import ResidualVQ
-
+from vector_quantize_pytorch.random_projection_quantizer import RandomProjectionQuantizer
---
+++ vector_quantize_pytorch/random_projection_quantizer.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+from vector_quantize_pytorch.vector_quantize_pytorch import VectorQuantize
+
+from einops import rearrange, repeat, pack, unpack
+
+class RandomProjectionQuantizer(nn.Module):
+    """ https://arxiv.org/abs/2202.01855 """
+
+    def __init__(
+        self,
+        *,
+        dim,
+        codebook_size,
+        codebook_dim,
+        num_codebooks = 1,
+        **kwargs
+    ):
+        super().__init__()
+        self.num_codebooks = num_codebooks
+
+        rand_projs = torch.empty(num_codebooks, dim, codebook_dim)
+        nn.init.xavier_normal_(rand_projs)
+
+        self.register_buffer('rand_projs', rand_projs)
+
+        self.vq = VectorQuantize(
+            dim = codebook_dim * num_codebooks,
+            heads = num_codebooks,
+            codebook_size = codebook_size,
+            use_cosine_sim = True,
+            separate_codebook_per_head = True,
+            **kwargs
+        )
+
+    def forward(self, x):
+
+        x = einsum('b n d, h d e -> b n h e', x, self.rand_projs)
+        x, ps = pack([x], 'b n *')
+
+        self.vq.eval()
+        _, indices, _ = self.vq(x)
+
+        return indices
