d3734638af81506c7eb442c098e0287e778c2046
fluffy <sekstinilol@gmail.com>
2023-09-29 00:03:12 +0200

    Make it importable

--- vector_quantize_pytorch/__init__.py
+++ vector_quantize_pytorch/__init__.py
@@ -1,3 +1,4 @@
 from vector_quantize_pytorch.vector_quantize_pytorch import VectorQuantize
 from vector_quantize_pytorch.residual_vq import ResidualVQ, GroupedResidualVQ
 from vector_quantize_pytorch.random_projection_quantizer import RandomProjectionQuantizer
+from vector_quantize_pytorch.finite_scalar_quantization import FSQ
