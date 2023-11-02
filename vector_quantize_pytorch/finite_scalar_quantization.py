1dc2f12c15c070504c0b52293819aaa0505bad7f
lucidrains <lucidrains@gmail.com>
2023-11-02 10:15:50 -0700

    take care of input dimension being different than codebook dimension for FSQ

--- vector_quantize_pytorch/finite_scalar_quantization.py
+++ vector_quantize_pytorch/finite_scalar_quantization.py
@@ -7,33 +7,54 @@ from typing import List
 
 import torch
 import torch.nn as nn
+from torch.nn import Module
 from torch import Tensor, int32
 
+# helper functions
+
+def exists(v):
+    return v is not None
+
+def default(*args):
+    for arg in args:
+        if exists(arg):
+            return arg
+    return None
+
+# tensor helpers
 
 def round_ste(z: Tensor) -> Tensor:
     """Round with straight through gradients."""
     zhat = z.round()
     return z + (zhat - z).detach()
 
+# main class
 
-class FSQ(nn.Module):
-    def __init__(self, levels: List[int]):
+class FSQ(Module):
+    def __init__(
+        self,
+        levels: List[int],
+        dim = None
+    ):
         super().__init__()
         _levels = torch.tensor(levels, dtype=int32)
         self.register_buffer("_levels", _levels)
 
+        self.dim = default(dim, len(_levels))
+
         _basis = torch.cumprod(torch.tensor([1] + levels[:-1]), dim=0, dtype=int32)
         self.register_buffer("_basis", _basis)
 
-        self.dim = len(levels)
+        codebook_dim = len(levels)
+        self.codebook_dim = codebook_dim
+
+        self.dim = default(dim, codebook_dim)
+        self.project_in = nn.Linear(self.dim, codebook_dim) if self.dim != codebook_dim else nn.Identity()
+        self.project_out = nn.Linear(codebook_dim, self.dim) if self.dim != codebook_dim else nn.Identity()
+
         self.n_codes = self._levels.prod().item()
         implicit_codebook = self.indices_to_codes(torch.arange(self.n_codes))
         self.register_buffer("implicit_codebook", implicit_codebook)
-
-    def forward(self, z: Tensor) -> Tensor:
-        zhat = self.quantize(z)
-        indices = self.codes_to_indices(zhat)
-        return zhat, indices
 
     def bound(self, z: Tensor, eps: float = 1e-3) -> Tensor:
         """Bound `z`, an array of shape (..., d)."""
@@ -58,12 +79,34 @@ class FSQ(nn.Module):
     
     def codes_to_indices(self, zhat: Tensor) -> Tensor:
         """Converts a `code` to an index in the codebook."""
-        assert zhat.shape[-1] == self.dim
+        assert zhat.shape[-1] == self.codebook_dim
         zhat = self._scale_and_shift(zhat)
         return (zhat * self._basis).sum(dim=-1).to(int32)
     
-    def indices_to_codes(self, indices: Tensor) -> Tensor:
+    def indices_to_codes(
+        self,
+        indices: Tensor,
+        project_out = True
+    ) -> Tensor:
         """Inverse of `codes_to_indices`."""
+
         indices = indices.unsqueeze(-1)
         codes_non_centered = (indices // self._basis) % self._levels
-        return self._scale_and_shift_inverse(codes_non_centered)
+        codes = self._scale_and_shift_inverse(codes_non_centered)
+
+        if not project_out:
+            return codes
+
+        return self.project_out(codes)
+
+    def forward(self, z: Tensor) -> Tensor:
+        assert z.shape[-1] == self.dim, f'expected dimension of {self.dim} but found dimension of {z.shape[-1]}'
+
+        z = self.project_in(z)
+
+        codes = self.quantize(z)
+        indices = self.codes_to_indices(codes)
+
+        out = self.project_out(codes)
+
+        return out, indices
