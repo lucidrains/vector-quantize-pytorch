078f005125dc969296cd292b1ab136b4625e635c
lucidrains <lucidrains@gmail.com>
2023-12-16 06:42:41 -0800

    1.12.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'vector_quantize_pytorch',
   packages = find_packages(),
-  version = '1.12.2',
+  version = '1.12.4',
   license='MIT',
   description = 'Vector Quantization - Pytorch',
   long_description_content_type = 'text/markdown',
