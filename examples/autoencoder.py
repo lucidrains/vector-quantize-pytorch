5b9d87142a0ee80c2259733acaea1dd185d9f819
Baptiste Morisse <bmorisse.pro@proton.me>
2024-05-28 18:58:23 +0200

    Working on the examples. Use Lightning to provide a simple way of training all three examples. Add a data.py which contains the FashionMNIST code.

--- examples/autoencoder.py
+++ examples/autoencoder.py
@@ -1,13 +1,15 @@
 # FashionMnist VQ experiment with various settings.
 # From https://github.com/minyoungg/vqtorch/blob/main/examples/autoencoder.py
 
-from lightning import LightningDataModule, LightningModule, Trainer, seed_everything
+import torch
+from lightning import LightningModule, Trainer, seed_everything
+from lightning.pytorch.callbacks import RichModelSummary, RichProgressBar
 from torch import nn
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import FashionMNIST
-from torchvision.transforms import v2
+from torch.nn.functional import l1_loss
 
 from vector_quantize_pytorch import VectorQuantize
+
+from .data import LitFashionMNIST
 
 seed_everything(1234, workers=True)
 
@@ -26,28 +28,22 @@ class SimpleVQAutoEncoder(LightningModule):
         self.alpha = alpha
         self.lr = lr
 
-        self.encoder = nn.ModuleList(
-            [
-                nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-                nn.GELU(),
-                nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-            ]
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.GELU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
-        self.decoder = nn.ModuleList(
-            [
-                nn.Upsample(scale_factor=2, mode="nearest"),
-                nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
-                nn.GELU(),
-                nn.Upsample(scale_factor=2, mode="nearest"),
-                nn.Conv2d(16, 1, kernel_size=3, stride=1, padding=1),
-            ]
+        self.decoder = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="nearest"),
+            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
+            nn.GELU(),
+            nn.Upsample(scale_factor=2, mode="nearest"),
+            nn.Conv2d(16, 1, kernel_size=3, stride=1, padding=1),
         )
-        self.quantizer = (
-            VectorQuantize(
-                dim=dim, codebook_size=codebook_size, accept_image_fmap=True
-            ),
+        self.quantizer = VectorQuantize(
+            dim=dim, codebook_size=codebook_size, accept_image_fmap=True
         )
 
     def forward(self, x):
@@ -58,8 +54,9 @@ class SimpleVQAutoEncoder(LightningModule):
         return out.clamp(-1, 1), indices, commit_loss
 
     def training_step(self, batch, batch_idx):
-        out, indices, commit_loss = self.forward(batch)
-        rec_loss = (out - batch).abs().mean()
+        input, _ = batch
+        out, indices, commit_loss = self.forward(input)
+        rec_loss = l1_loss(out, input)
         loss = rec_loss + self.alpha * commit_loss
         self.log("train_loss", loss, prog_bar=True)
         self.log("train_commit_loss", commit_loss, prog_bar=True)
@@ -70,8 +67,9 @@ class SimpleVQAutoEncoder(LightningModule):
         )
 
     def validation_step(self, batch, batch_idx):
-        out, indices, commit_loss = self.forward(batch)
-        rec_loss = (out - batch).abs().mean()
+        input, _ = batch
+        out, indices, commit_loss = self.forward(input)
+        rec_loss = l1_loss(out, input)
         loss = rec_loss + self.alpha * commit_loss
         self.log("val_loss", loss, prog_bar=True)
         self.log("val_commit_loss", commit_loss, prog_bar=True)
@@ -81,33 +79,15 @@ class SimpleVQAutoEncoder(LightningModule):
             prog_bar=True,
         )
 
-
-class LitFashionMNIST(LightningDataModule):
-    def __init__(self, data_dir, batch_size: int = 256):
-        super().__init__()
-        self.data_dir = data_dir
-        self.batch_size = batch_size
-        self.transforms = v2.Compose([v2.ToTensor(), v2.Normalize((0.5,), (0.5,))])
-
-    def prepare_data(self):
-        # download
-        FashionMNIST(self.data_dir, train=True, download=True)
-
-    def setup(self, stage: str):
-        # Assign train/val datasets for use in dataloaders
-        if stage == "fit":
-            mnist_full = FashionMNIST(self.data_dir, transform=self.transforms)
-            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
-
-    def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.batch_size)
-
-    def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=self.batch_size)
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
+        return optimizer
 
 
 model = SimpleVQAutoEncoder()
-data = FashionMNIST()
-trainer = Trainer(logger=True, max_epochs=100)
+data = LitFashionMNIST()
+trainer = Trainer(
+    logger=True, max_epochs=100, callbacks=[RichProgressBar(), RichModelSummary()]
+)
 
 trainer.fit(model=model, datamodule=data)
---
+++ examples/data.py
@@ -0,0 +1,31 @@
+# FashionMnist VQ experiment with various settings.
+# From https://github.com/minyoungg/vqtorch/blob/main/examples/autoencoder.py
+
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import FashionMNIST
+from torchvision.transforms import v2
+
+
+class LitFashionMNIST(LightningDataModule):
+    def __init__(self, data_dir, batch_size: int = 256):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.transforms = v2.Compose([v2.ToTensor(), v2.Normalize((0.5,), (0.5,))])
+
+    def prepare_data(self):
+        # download
+        FashionMNIST(self.data_dir, train=True, download=True)
+
+    def setup(self, stage: str):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit":
+            mnist_full = FashionMNIST(self.data_dir, transform=self.transforms)
+            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=self.batch_size)
