9dfd02460ae5b1a6c070ba5b40c19ee5f6a93653
Baptiste Morisse <bmorisse.pro@proton.me>
2024-06-19 16:43:15 +0200

    Add a dedicated test suite for FSQ. For now, test only FSQ with or without returning indices.

---
+++ tests/test_finite_scalar_quantization.py
@@ -0,0 +1,33 @@
+import pytest
+import torch
+
+from vector_quantize_pytorch.finite_scalar_quantization import FSQ
+
+
+class TestFSQ:
+    levels = [8,5,5,5]
+    quantizer = FSQ(levels)
+
+    def test_init(self):
+        assert self.quantizer
+
+    def test_forward(self):
+        features = torch.randn(1, 1024, 4) # 4 since there are 4 levels
+        quantized, indices = self.quantizer(features)
+
+        assert quantized.shape == features.shape
+        assert torch.all(quantized == self.quantizer.indices_to_codes(indices))
+
+
+class TestFSQNoIndices:
+    levels = [8,5,5,5]
+    quantizer = FSQ(levels, return_indices=False)
+
+    def test_init(self):
+        assert self.quantizer
+
+    def test_forward(self):
+        features = torch.randn(1, 1024, 4) # 4 since there are 4 levels
+        _, indices = self.quantizer(features)
+
+        assert indices is None
