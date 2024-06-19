2767591ee03b0aeedd811c597749becd0f0502ad
Baptiste Morisse <bmorisse.pro@proton.me>
2024-06-19 11:53:59 +0200

    Change the default of codebook_size, discarding the use of lambda function to a simple use of '2**dim' instead. Hence a single, simple 'default' function.

--- vector_quantize_pytorch/lookup_free_quantization.py
+++ vector_quantize_pytorch/lookup_free_quantization.py
@@ -74,7 +74,7 @@ class LFQ(Module):
         assert exists(dim) or exists(codebook_size), 'either dim or codebook_size must be specified for LFQ'
         assert not exists(codebook_size) or log2(codebook_size).is_integer(), f'your codebook size must be a power of 2 for lookup free quantization (suggested {2 ** ceil(log2(codebook_size))})'
 
-        codebook_size = default(codebook_size, lambda: 2 ** dim)
+        codebook_size = default(codebook_size, 2 ** dim)
         self.codebook_size = codebook_size
 
         codebook_dim = int(log2(codebook_size))
