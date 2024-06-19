fc8ae6cd36cd42ac74331e074f56e684b93249b0
Baptiste Morisse <bmorisse.pro@proton.me>
2024-06-19 17:12:45 +0200

    Add a test for 'channels_first = True' attribute.

--- tests/test_finite_scalar_quantization.py
+++ tests/test_finite_scalar_quantization.py
@@ -38,16 +38,24 @@ class TestFSQNoIndices:
 
         assert indices is None
 
-class TestFSQWithDim:
+class TestFSQWithDimAndChannelFirst:
     levels = [8,5,5,5]
     dim = 16
-    quantizer = FSQ(levels, dim=dim)
+    quantizer = FSQ(levels, dim=dim, channel_first=True)
 
     def test_init(self):
         assert self.quantizer
 
     def test_forward(self):
-        image_feats = torch.randn(1, 16, 32, 32)
+        features = torch.randn(1, self.dim, 1024) # 4 since there are 4 levels
+        quantized, indices = self.quantizer(features)
+
+        assert quantized.shape == features.shape
+        assert torch.all(quantized == self.quantizer.indices_to_codes(indices))
+
+
+    def test_forward_images(self):
+        image_feats = torch.randn(1, self.dim, 32, 32)
         quantized, indices = self.quantizer(image_feats)
 
         assert quantized.shape == image_feats.shape
