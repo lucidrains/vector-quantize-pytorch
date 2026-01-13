eecfdc9ca215042bad675cc99d0ccc85bab25cd1
lucidrains <lucidrains@gmail.com>
2026-01-13 05:28:42 -0800

    move application of noise when noise dropout is on for fsq

--- vector_quantize_pytorch/finite_scalar_quantization.py
+++ vector_quantize_pytorch/finite_scalar_quantization.py
@@ -159,16 +159,19 @@ class FSQ(Module):
     def quantize(self, z):
         """ Quantizes z, returns quantized zhat, same shape as z. """
 
-        shape, device, noise_dropout, preserve_symmetry = z.shape[0], z.device, self.noise_dropout, self.preserve_symmetry
+        shape, device, preserve_symmetry = z.shape[0], z.device, self.preserve_symmetry
         bound_fn = self.symmetry_preserving_bound if preserve_symmetry else self.bound
 
-        bounded_z = bound_fn(z, hard_clamp = self.bound_hard_clamp)
+        return bound_fn(z, hard_clamp = self.bound_hard_clamp)
+
+    def maybe_apply_noise(self, bounded_z):
+        noise_dropout = self.noise_dropout
+
+        if not self.training or noise_dropout == 0.:
+            return
 
         # determine where to add a random offset elementwise
         # if using noise dropout
-
-        if not self.training or noise_dropout == 0.:
-            return bounded_z
 
         offset_mask = torch.full_like(bounded_z, noise_dropout).bernoulli_().bool()
         offset = torch.rand_like(bounded_z) - 0.5
@@ -269,6 +272,8 @@ class FSQ(Module):
 
             if self.return_indices:
                 indices = self.codes_to_indices(codes)
+
+            codes = self.maybe_apply_noise(codes)
 
             codes = rearrange(codes, 'b n c d -> b n (c d)')
 
